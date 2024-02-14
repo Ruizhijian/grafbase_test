b228897632a7a06bbe4e4b0fa4eaa85f42327825
Michael Gao <455192656@qq.com>
2024-02-15 02:00:57 +0800

    for grafbase test

---
+++ grafbase.config.ts
@@ -0,0 +1,32 @@
+import {g, auth, config} from '@grafbase/sdk'
+
+const User = g.model('User', {
+    email: g.string().unique(),
+    password: g.string(),
+    name: g.string().length({min: 3, max: 50}),
+    bio: g.string().optional(),
+    avatar: g.url(),
+    description: g.string().optional(),
+    githubUrl: g.url().optional(),
+  linkedInUrl: g.url().optional(),
+  projects:g.relation(()=>Project).list().optional()
+    })
+
+const Project = g.model('Project', {
+  title: g.string().length({min: 3, max: 50}),
+  description: g.string().length({min: 3, max: 200}),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+  category:g.string().search(),
+  createdBy:g.relation(()=>User)
+})
+
+export default config({
+    models: {User, Project},
+    auth: {
+        user: auth.user({
+        model: User
+        })
+    }
+    })
